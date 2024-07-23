0e85684fcafe85d0c8c6a89c802372a0b426dd4d
Nikolaev Anton <nikolaev.anton@gmail.com>
2024-07-23 07:54:16 +0300

    правки по ревью

--- L04-generics/src/main/java/homework/CustomerService.java
+++ L04-generics/src/main/java/homework/CustomerService.java
@@ -4,24 +4,24 @@ import java.util.*;
 
 public class CustomerService {
 
-    private final NavigableMap<Customer, String> customerStringTreeMap = new TreeMap<>();
+    private final NavigableMap<Customer, String> customerStringNavigableMap = new TreeMap<>();
 
     public Map.Entry<Customer, String> getSmallest() {
-        return getCustomerStringSimpleEntry(customerStringTreeMap.firstEntry());
+        return getCustomerStringSimpleEntry(customerStringNavigableMap.firstEntry());
     }
 
     public Map.Entry<Customer, String> getNext(Customer customer) {
-        if (customerStringTreeMap.higherEntry(customer) == null) {
+        if (customerStringNavigableMap.higherEntry(customer) == null) {
             return null;
         }
-        return getCustomerStringSimpleEntry(customerStringTreeMap.higherEntry(customer));
+        return getCustomerStringSimpleEntry(customerStringNavigableMap.higherEntry(customer));
     }
 
     public void add(Customer customer, String data) {
-        customerStringTreeMap.put(customer, data);
+        customerStringNavigableMap.put(customer, data);
     }
 
     private AbstractMap.SimpleEntry<Customer, String> getCustomerStringSimpleEntry(Map.Entry<Customer, String> entry) {
-        return new AbstractMap.SimpleEntry<>(new Customer(entry.getKey()),entry.getValue());
+        return new AbstractMap.SimpleEntry<>(new Customer(entry.getKey()), entry.getValue());
     }
 }
